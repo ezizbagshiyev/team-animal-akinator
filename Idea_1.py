170e80fa4afaf6f442081ab2ea856c3a937dc4fe
Larry Luther <larryluther21@gmail.com>
2024-07-21 13:37:20 -0400

    Very limited and basic but working version of the game

---
+++ Idea_1.py
@@ -0,0 +1,137 @@
+dict = [
+    {"animal":"Dog", "pet":True, "4_legs":True, "water":False, "wings":False, "feathers":False, "size":True, "swim":True, "petstore":True, "trees":False, "reptile":False, "eaten":False, "poisonous":False, "webs":False, "predator":False, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivore":False, "stand":False, "roar":False, "bark":True, "long_ears":False},
+
+    {"animal":"Cat", "pet":True, "4_legs":True, "water":False, "wings":False, "feathers":False, "size":True, "swim":True, "petstore":True, "trees":False, "reptile":False, "eaten":False, "poisonous":False, "webs":False, "predator":True, "pouch":False, "sharp_teeth":True, "nocturnal":False, "ice":False, "carnivore":True, "stand":False, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Cow", "pet":False, "4_legs":True, "water":False, "wings":False, "feathers":False, "size":False, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":True, "poisonous":False, "webs":False, "predator":False, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivore":False, "stand":False, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Horse", "pet":True, "4_legs":True, "water":False, "wings":False, "feathers":False, "size":False, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":False, "poisonous":False, "webs":False, "predator":False, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivore":False, "stand":False, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Chicken", "pet":True, "4_legs":False, "water":False, "wings":True, "feathers":True, "size":True, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":True, "poisonous":False, "webs":False, "predator":False, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivore":False, "stand":True, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Duck", "pet":False, "4_legs":False, "water":False, "wings":True, "feathers":True, "size":True, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":True, "poisonous":False, "webs":False, "predator":False, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivore":False, "stand":True, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Rabbit", "pet":True, "4_legs":True, "water":False, "wings":False, "feathers":False, "size":True, "swim":True, "petstore":True, "trees":False, "reptile":False, "eaten":True, "poisonous":False, "webs":False, "predator":False, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivore":False, "stand":False, "roar":False, "bark":False, "long_ears":True},
+
+    {"animal":"Elephant", "pet":False, "4_legs":True, "water":False, "wings":False, "feathers":False, "size":False, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":False, "poisonous":False, "webs":False, "predator":False, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivore":False, "stand":False, "roar":False, "bark":False, "long_ears":True},
+
+    {"animal":"Lion", "pet":False, "4_legs":True, "water":False, "wings":False, "feathers":False, "size":False, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":False, "poisonous":False, "webs":False, "predator":True, "pouch":False, "sharp_teeth":True, "nocturnal":False, "ice":False, "carnivore":True, "stand":False, "roar":True, "bark":False, "long_ears":False},
+
+    {"animal":"Kangaroo", "pet":False, "4_legs":False, "water":False, "wings":False, "feathers":False, "size":False, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":False, "poisonous":False, "webs":False, "predator":False, "pouch":True, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivore":False, "stand":True, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Bear", "pet":False, "4_legs":True, "water":False, "wings":False, "feathers":False, "size":False, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":False, "poisonous":False, "webs":False, "predator":True, "pouch":False, "sharp_teeth":True, "nocturnal":False, "ice":False, "carnivore":False, "stand":False, "roar":True, "bark":False, "long_ears":False},
+
+    {"animal":"Deer", "pet":False, "4_legs":True, "water":False, "wings":False, "feathers":False, "size":False, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":True, "poisonous":False, "webs":False, "predator":False, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivor":False, "stand":False, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Owl", "pet":False, "4_legs":False, "water":False, "wings":True, "feathers":True, "size":True, "swim":False, "petstore":False, "trees":True, "reptile":False, "eaten":False, "poisonous":False, "webs":False, "predator":True, "pouch":False, "sharp_teeth":False, "nocturnal":True, "ice":False, "carnivore":True, "stand":True, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Penguin", "pet":False, "4_legs":False, "water":False, "wings":True, "feathers":True, "size":True, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":False, "poisonous":False, "webs":False, "predator":False, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":True, "carnivore":False, "stand":True, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Shark", "pet":False, "4_legs":False, "water":True, "wings":False, "feathers":False, "size":False, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":False, "poisonous":False, "webs":False, "predator":True, "pouch":False, "sharp_teeth":True, "nocturnal":False, "ice":False, "carnivore":True, "stand":False, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Snake", "pet":True, "4_legs":False, "water":False, "wings":False, "feathers":False, "size":True, "swim":True, "petstore":True, "trees":True, "reptile":True, "eaten":False, "poisonous":True, "webs":False, "predator":True, "pouch":False, "sharp_teeth":True, "nocturnal":False, "ice":False, "carnivore":True, "stand":False, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Alligator", "pet":False, "4_legs":True, "water":True , "wings":False, "feathers":False, "size":False, "swim":True, "petstore":False, "trees":False, "reptile":True, "eaten":True, "poisonous":False, "webs":False, "predator":True, "pouch":False, "sharp_teeth":True, "nocturnal":False, "ice":False, "carnivore":True, "stand":False, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Pig", "pet":False, "4_legs":True, "water":False, "wings":False, "feathers":False, "size":False, "swim":True, "petstore":False, "trees":False, "reptile":False, "eaten":True, "poisonous":False, "webs":False, "predator":False, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivore":False, "stand":False, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Spider", "pet":True, "4_legs":False, "water":False, "wings":False, "feathers":False, "size":True, "swim":False, "petstore":True, "trees":True, "reptile":False, "eaten":False, "poisonous":False, "webs":True, "predator":True, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivore":False, "stand":False, "roar":False, "bark":False, "long_ears":False},
+
+    {"animal":"Frog", "pet":True, "4_legs":False, "water":True, "wings":False, "feathers":False, "size":True, "swim":True, "petstore":True, "trees":True, "reptile":True, "eaten":False, "poisonous":False, "webs":False, "predator":False, "pouch":False, "sharp_teeth":False, "nocturnal":False, "ice":False, "carnivor":False, "stand":False, "roar":False, "bark":False, "long_ears":False},
+
+]
+
+def ask_question(answer, attribute):
+    if answer == "y":
+        ans = True
+    else:
+        ans = False
+
+    to_remove=[]
+    for d in dict:
+        if d[attribute] != ans:
+            to_remove.append(d)
+
+    for i in to_remove:
+        dict.remove(i)
+
+    if len(dict) == 1:
+        print("The anmimal you are thinkin of is: "+dict[0]["animal"])
+        quit()
+    if len(dict) == 0:
+        print("I am sorry, I could not guess your animal.")
+        quit()
+
+print("Please think of an animal and I will try to guess it.")
+
+ans = input("Is your animal a pet?(y,n)")
+ask_question(ans, "pet")
+
+ans = input("Does your animal have four legs?(y,n)")
+ask_question(ans, "4_legs")
+
+ans = input("Does your animal live in the water?(y,n)")
+ask_question(ans,"water")
+
+ans = input("Does your animal have wings?(y,n)")
+ask_question(ans,"wings")
+
+ans = input("Does your animal have feathers?(y,n)")
+ask_question(ans,"feathers")
+
+ans = input("Is your animal is smaller than a human?(y,n)")
+ask_question(ans,"size")
+
+ans = input("Can your animal swim?(y,n)")
+ask_question(ans,"swim")
+
+ans = input("Can your animal be found in pet stores?y,n)")
+ask_question(ans,"petstore")
+
+ans = input("Does your animal live in trees?(y,n)")
+ask_question(ans,"trees")
+
+ans = input("Is your animal a reptile?(y,n)")
+ask_question(ans,"reptile")
+
+ans = input("Is your animal often eaten by humans?(y,n)")
+ask_question(ans,"eaten")
+
+ans = input("Is your animal poisonous?(y,n)")
+ask_question(ans,"poisonous")
+
+ans = input("Can it create webs?(y,n)")
+ask_question(ans,"webs")
+
+ans = input("Is your animal a predator?(y,n)")
+ask_question(ans,"predator")
+
+ans = input("Does your animal have a pouch?(y,n)")
+ask_question(ans,"pouch")
+
+ans = input("Does your animal have sharp teeth?(y,n)")
+ask_question(ans,"sharp_teeth")
+
+ans = input("Is your animal nocturnal?(y,n)")
+ask_question(ans,"nocturnal")
+
+ans = input("Does your animal live on the ice?(y,n)")
+ask_question(ans,"ice")
+
+ans = input("Is your animal a carnivore?(y,n)")
+ask_question(ans,"carnivore")
+
+ans = input("Can your animal stand on two legs?(y,n)")
+ask_question(ans,"stand")
+
+ans = input("Does your animal roar?(y,n)")
+ask_question(ans,"roar")
+
+ans = input("Does your animal bark?(y,n)")
+ask_question(ans,"bark")
+
+ans = input("Does your animal have long ears?(y,n)")
+ask_question(ans,"long_ears")
+
+
+
